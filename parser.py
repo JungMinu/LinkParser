8751c0071b3fc4a4df2a0b178b9769a96a24e7a9
Minwoo Jung <jmwsoft@gmail.com>
2015-08-26 20:43:34 +0900

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,11 +1,1 @@
-# implemented by jmwsoft@gmail.com
-from bs4 import BeautifulSoup
-import urllib2
 
-resp = urllib2.urlopen("")
-soup = BeautifulSoup(resp, from_encoding="")
-fo = open("result.txt", "w")
-result = ""
-for link in soup.find_all('a', href=True):
-    result += link['href']+':'+link.text+"\n"
-fo.write(result.encode('utf8') + '\n')
